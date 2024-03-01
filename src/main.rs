d834a16903c3cf58c48eb60b79c4bc156ab9ffaf
Serega <26937163+serega-fighter@users.noreply.github.com>
2024-03-01 18:31:43 +0300

    test shared state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
+}
 
 // state of the app
 struct AppState {
     app_name: String,
 }
 
-#[get("/")]
+/*#[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
+}*/
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -19,23 +24,25 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
-            .service(hello)
+            .app_data(counter.clone())
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/", web::get().to(index))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
